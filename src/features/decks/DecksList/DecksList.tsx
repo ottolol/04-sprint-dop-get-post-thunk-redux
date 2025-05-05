698d660c4b3e16be34eb8fd0c276ee429d02957a
ottolol <alexsveshnik@gmail.com>
2025-05-05 22:28:37 +0300

    Вынесли логику получения decks в собственный ХУК - useFetchDeck

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,16 @@
 import s from './DecksList.module.css'
+import { DeckItem } from './DeckItem/DeckItem'
+import { useFetchDeck } from './useFetchDecks'
 
 export const DecksList = () => {
-  return <ul className={s.list}></ul>
+  // Вынесли логику получения decks в собственный ХУК - useFetchDeck
+  const { decks } = useFetchDeck()
+
+  return (
+    <ul className={s.list}>
+      {decks.map((deck) => (
+        <DeckItem key={deck.id} deck={deck} />
+      ))}
+    </ul>
+  )
 }
