038e21cd25abd443aabffb9194109acd9ada5b71
ottolol <alexsveshnik@gmail.com>
2025-05-05 22:26:31 +0300

    Протипизировали initialState, написали 2 экшена: setDecksAC и addDeckAC, создали case

--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -1,5 +1,7 @@
+import { Deck } from './decks-api.ts'
+
 const initialState = {
-  decks: [] as any[], // todo: add type
+  decks: [] as Deck[],
   searchParams: {
     name: '',
   },
@@ -9,9 +11,31 @@ type DecksState = typeof initialState
 
 export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
   switch (action.type) {
+    case 'DECKS/SET-DECKS':
+      return {
+        ...state,
+        decks: action.decks,
+      }
+
+    case 'DECKS/ADD-DECK':
+      return {
+        ...state,
+        decks: [action.deck, ...state.decks],
+      }
+
     default:
       return state
   }
 }
 
-type DecksActions = any
+type DecksActions = ReturnType<typeof setDecksAC> | ReturnType<typeof addDeckAC>
+
+export const setDecksAC = (decks: Deck[]) => ({
+  type: 'DECKS/SET-DECKS' as const,
+  decks,
+})
+
+export const addDeckAC = (deck: Deck) => ({
+  type: 'DECKS/ADD-DECK' as const,
+  deck,
+})
