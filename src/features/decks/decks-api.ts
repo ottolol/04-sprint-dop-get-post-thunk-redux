5cc5da5bf70160722ca9d82a6e1cc7bad3234e84
ottolol <alexsveshnik@gmail.com>
2025-05-05 22:25:31 +0300

    Написали 2 запроса fetchDecks и addDeck и протипизировали

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,45 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksAPI = {
+  fetchDecks() {
+    return instance.get<FetchDecksResponse>(`/v2/decks`)
+  },
+  addDeck(params: AddDecksParams) {
+    return instance.post<Deck>(`/v1/decks`, params)
+  },
+}
+
+export type AddDecksParams = {
+  name: string
+}
+
+export type FetchDecksResponse = {
+  items: Deck[]
+  pagination: Pagination
+  maxCardsCount: number
+}
+export type Author = {
+  id: string
+  name: string
+}
+export type Deck = {
+  author: Author
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string
+  rating: number
+  created: string
+  updated: string
+  cardsCount: number
+}
+export type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
